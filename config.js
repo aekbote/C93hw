a5c880fa03bdc2d5ce1b001b85603d48da0f4417
aekbote <88759575+aekbote@users.noreply.github.com>
2022-12-30 15:55:36 -0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBU8CiYD8633ujhwJZtTd0b7Aawgf3jfSQ",
+    authDomain: "spectagram-app-f34c2.firebaseapp.com",
+    databaseURL: "https://spectagram-app-f34c2-default-rtdb.firebaseio.com",
+    projectId: "spectagram-app-f34c2",
+    storageBucket: "spectagram-app-f34c2.appspot.com",
+    messagingSenderId: "196370653532",
+    appId: "1:196370653532:web:d4e7da44016a8d52fea199"
+  };
+  
